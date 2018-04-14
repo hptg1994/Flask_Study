e16444e06b518f7c3fb0e579eb86a42ec45b051c
hptg1994 <hptg1994@gmail.com>
2018-04-14 16:01:06 -0400

    regular update

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template
+from flask import Flask,render_template, session, redirect, url_for, flash
 from flask_script import Manager
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
@@ -18,14 +18,17 @@ class NameForm(Form):
     name = StringField('What is your name?',validators=[Required()])
     submit = SubmitField('Submit')
 
-@app.route('/', method=['GET','POST'])
+@app.route('/', methods=['GET','POST'])
 def index():
-    name = None
     form = NameForm()
     if form.validate_on_submit():
-        name = form.name.data
-        form.name.data = ''
-    return render_template('index.html', form = form, name=name, current_time=datetime.utcnow())
+        old_name = session.get('name')
+        if old_name is not None and old_name != form.name.data:
+            flash('Looks like you have change your name!')
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
+    return render_template('index.html', form = form, name=session.get('name'), current_time=datetime.utcnow())
+
 
 @app.route('/user/<name>')
 def hello(name=None):
